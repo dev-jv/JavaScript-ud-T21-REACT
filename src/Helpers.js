84527f96b283bcd05906333b28e1d7f040b60aac
dev-jv <dev.javierleon@gmail.com>
2020-11-04 00:09:33 -0500

    ten

--- src/Helpers.js
+++ src/Helpers.js
@@ -13,6 +13,29 @@ export function calcularTotal(cantidad, plazo) {
     }
 
     console.log(totalCantidad);
+
+
+    let totalPlazo = 0;
+
+    switch(plazo) {
+        case 3:
+            totalPlazo = cantidad * .05;
+            break;
+        case 6:
+            totalPlazo = cantidad * .10;
+            break;
+        case 12:
+            totalPlazo = cantidad * .15;
+            break;
+        case 24:
+            totalPlazo = cantidad * .20;
+            break;
+        default:
+            break;  
+    }
+
+    // console.log(totalPlazo);
+    return totalPlazo + totalCantidad + cantidad;
 }
 
 
